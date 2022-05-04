1f08e666170896f5d242c35a8f2ba0e1208c22fd
Dmitriy <tefromhell@gmail.com>
2022-05-04 18:58:25 +0300

    f

--- src/components/End.jsx
+++ src/components/End.jsx
@@ -5,7 +5,7 @@ function End({fetchData}) {
   return (
     <div className='end'>
         <div className='end__window'>
-        <b>You match all Pokemon</b>
+        <b>You matched all Pokemon</b>
         <button onClick={fetchData} >Play again?</button>
         </div>
     </div>
